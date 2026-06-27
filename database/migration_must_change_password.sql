b04f436d4a079edde287b66e4a7bbdbba4e06d56
alonvaknin <aloncomputer@gmail.com>
2026-06-27 23:35:38 +0300

    רענון תיקות וקבצים

--- database/migration_must_change_password.sql
+++
@@ -1,6 +0,0 @@
--- Migration: add must_change_password column to users table
--- Run once on the production DB
-
-ALTER TABLE users
-  ADD COLUMN must_change_password TINYINT(1) NOT NULL DEFAULT 0
-  AFTER password_hash;
